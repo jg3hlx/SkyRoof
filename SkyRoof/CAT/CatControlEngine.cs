f7f989cfc58c3a1b722ec0cb5822a4f04d279ddb
VE3NEA <alshovk@dxatlas.com>
2025-08-07 16:08:31 -0400

    keep merging

--- SkyRoof/CAT/CatControlEngine.cs
+++ SkyRoof/CAT/CatControlEngine.cs
@@ -282,6 +282,8 @@ namespace SkyRoof
       string command = GetWriteRxModeCommand();
       if (command == string.Empty) return;
 
+      string mode = $"{RequestedRxMode}".Replace("_D", "");
+      string command = RadioInfo.commands.set_main_mode!.Replace("{mode}", mode);
       SendWriteCommand(command);
       LastWrittenRxMode = RequestedRxMode;
     }
