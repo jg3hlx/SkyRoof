c35c59e7c6939620046a14e94d3c4cb05dd93981
VE3NEA <alshovk@dxatlas.com>
2025-12-31 13:53:08 -0500

    sequencer wip

---
+++ SkyRoof/FT4/Ft4QsoSequencer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace SkyRoof
+{
+  // meaningful names for the WsjtxQsoState enum
+  public enum Ft4MessageType {Unknown, DE, dB, RR_dB, RR73, _73, CQ }
+
+  internal class Ft4QsoSequencer
+  {
+    private string mySquare;
+    private string myCall;
+    private string?[] messages = new string[7];
+
+    public string MyCall { get => myCall; set => SetMyCall(value); }
+    public string MySquare{ get => mySquare; set => SetMySquare(value); }
+    public string? HisCall { get; private set; }
+    public string? HisSquare { get; private set; }
+    public int? MyReport { get; private set; }
+    public int? HisReport { get; private set; }
+
+    public Ft4MessageType MessageType {get; private set;}
+    public string? Message => messages[(int)MessageType];
+
+
+    public Ft4QsoSequencer(string call, string square, string myCall)
+    {
+      myCall = call;
+      mySquare = square;
+      GenerateMessages();
+    }
+
+    public void Reset()
+    {
+      HisCall = null;
+      HisSquare = null;
+      HisReport = null;
+      GenerateMessages();
+      MessageType = Ft4MessageType.CQ;
+    }
+
+    private void SetMyCall(string value)
+    {
+      myCall = value;
+      GenerateMessages();
+    }
+
+    private void SetMySquare(string value)
+    {
+      mySquare = value;
+      GenerateMessages();
+    }
+
+    // messages received
+    public bool ProcessMessages(IEnumerable<DecodedItem> items)
+    {
+
+      return false;
+    }
+
+    // user clicked on received message
+    public void ReplyToMessage(DecodedItem item)
+    {
+
+    }
+
+    // user clicked on a message type button
+    public void ForceMessage(Ft4MessageType messageType)
+    {
+
+    }
+
+    private void GenerateMessages()
+    {
+      messages[(int)Ft4MessageType.CQ] = $"CQ {myCall} {mySquare}";
+
+      if (HisCall != null)
+      {
+        messages[(int)Ft4MessageType.DE] = $"{HisCall} {myCall} {mySquare}";
+        messages[(int)Ft4MessageType.dB] = $"{HisCall} {myCall} {MyReport}";
+        messages[(int)Ft4MessageType.RR_dB] = $"{HisCall} {myCall} RR{MyReport}";
+        messages[(int)Ft4MessageType.RR73] = $"{HisCall} {myCall} RR73";
+        messages[(int)Ft4MessageType._73] = $"{HisCall} {myCall} 73";
+      }
+      else
+        for (Ft4MessageType i = Ft4MessageType.Unknown; i <= Ft4MessageType._73; i++) 
+          messages[(int)i] = null;
+    }
+  }
+}
